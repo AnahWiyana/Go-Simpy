574e979e50cd6dc0ea8603dd1fd1d49389c449f3
miawicey <vitakhairana@gmail.com>
2025-11-09 19:50:31 +0800

    Coba liat tampilan driver yg ak isi

--- Go-Simpy/tampilan.py
+++ Go-Simpy/tampilan.py
@@ -8,37 +8,61 @@ from autentikasi import dapatkan_data_akun
 from prettytable import PrettyTable
 
 def bersihkan_layar():
-	os.system('cls' if os.name == 'nt' else 'clear')
+    os.system('cls' if os.name == 'nt' else 'clear')
 
 def tampilkan_menu_login():
-	bersihkan_layar()
-	print("Menu Login")
-	print("1. Login")
-	print("2. Registrasi")
-	print("3. Keluar Program")
+    bersihkan_layar()
+    print("=== MENU LOGIN ===")
+    print("1. Login")
+    print("2. Registrasi")
+    print("3. Keluar Program")
 
 def tampilkan_menu_utama():
-	bersihkan_layar()
-	username = dapatkan_data_akun()
-	data_akun = akun[username]
-	print(f"Selamat datang, {data_akun['nama_lengkap']} ({username})")
+    bersihkan_layar()
+    username = dapatkan_data_akun()
+    data_akun = akun[username]
+    role = data_akun["role"]
 
-	if data_akun["role"] == "admin":
-		print("Anda adalah admin.")
-		print("\n=== MENU ADMIN ===")
-	
-	else:
-		print("\n=== MENU ===")
-    
-	if data_akun["role"] == "admin":
-		print("1. Tambah User")
-		print("2. Hapus User")
-		print("3. Lihat Daftar User")
-		print("4. Edit Profil")
-		print("5. Logout")
-		print("6. Keluar")
-    
-	else:
-		print("1. Edit Profil")
-		print("2. Logout")
-		print("3. Keluar")
+    print(f"Selamat datang, {data_akun['nama_lengkap']} ({role.capitalize()})")
+    print("=" * 40)
+
+    # ==== MENU ===========
+    if role == "admin":
+        print("=== MENU ADMIN ===")
+        print("1. Tambah User")
+        print("2. Hapus User")
+        print("3. Lihat Daftar User")
+        print("4. Edit Profil")
+        print("5. Logout")
+        print("6. Keluar Program")
+
+    elif role == "customer":
+        print("=== MENU CUSTOMER ===")
+        print("1. Pesan Layanan")
+        print("2. Lihat Riwayat Pesanan")
+        print("3. Edit Profil")
+        print("4. Logout")
+        print("5. Keluar Program")
+
+    elif role == "manager":
+        print("=== MENU MANAGER ===")
+        print("1. Naikkan Gaji Driver")
+        print("2. Grafik Penghasilan Driver")
+        print("3. Edit Profil")
+        print("4. Logout")
+        print("5. Keluar Program")
+
+    elif role == "driver":
+        print("=== MENU DRIVER ===")
+        print("1. Lihat Daftar Order Masuk")
+        print("2. Konfirmasi Order")
+        print("3. Lihat Penghasilan")
+        print("4. Edit Profil")
+        print("5. Logout")
+        print("6. Keluar Program")
+
+    else:
+        print("Role tidak dikenal!")
+
+    pilihan = input("\nPilih menu: ").strip()
+    return pilihan
